04bd0fd44374fbcd826a24ecb3d508b43dc36503
Wurberry <dany.evstigneev@gmail.com>
2021-04-23 00:24:31 +0300

    Доделана логика игры "Игрок и Игрок", сделан рейтинг на главном экране, полностью доделана бд, подписаны комментарии всех методов

--- db_scripts.py
+++ db_scripts.py
@@ -16,6 +16,10 @@ class datebase:
                     count_win INT,
                     count_lose INT);""")
 
+    """
+            Метод для проверки входа пользователя
+    """
+
     def checkUser(self, login, password):
         self.cur.execute(f"SELECT * FROM register WHERE login ='{login}' and password = '{password}';")
         res = self.cur.fetchone()
@@ -24,6 +28,10 @@ class datebase:
             return 0       # print("Такого пользователся нет, регистрируйтесь!!!!!")
         if res is not None:
             return 1       # print("Вы вошли, поздравляю")
+
+    """
+            Метод для регистрации входа пользователя
+    """
 
     def addUser(self, login, password):
         self.cur.execute(f"SELECT * FROM register WHERE login ='{login}';")
@@ -36,6 +44,10 @@ class datebase:
         if res is not None:
             print("Данный пользователь уже существует") # вывод окна с сообщением "Такой пользователь уже существует"
 
+    """
+            Метод для обновления пароля пользователя в бд
+    """
+
     def updateUserPassword(self, login, password):
         self.cur.execute(f"SELECT * FROM register WHERE login ='{login}';")
         if self.cur.fetchone() is not None:
@@ -43,20 +55,52 @@ class datebase:
         if self.cur.fetchone() is None:
             pass # вывод окна с сообщением "Неправильный логин"
 
+    """
+            Метод для сохранения результатов игры в рейтинг бд
+    """
+
     def setRecord(self, login, result): #result - победа (1) или поражение (0)
         if result == 1:
-            self.cur.execute(f"SELECT count_win FROM rating Where login = '{login}'")
+            self.cur.execute(f"SELECT count_win FROM rating WHERE login = '{login}'")
             count_win = self.cur.fetchone()
-            self.cur.execute(f"UPDATE rating SET count_win = '{result}'")
+            res = count_win[0] + 1
+            self.cur.execute(f"UPDATE rating SET count_win = '{res}' WHERE login = '{login}'")
+            self.conn.commit()
         if result == 0:
-            count_lose = self.cur.execute(f"SELECT count_lose FROM rating Where login = '{login}'")
+            self.cur.execute(f"SELECT count_lose FROM rating WHERE login = '{login}'")
+            count_lose = self.cur.fetchone()
+            res = count_lose[0] + 1
+            self.cur.execute(f"UPDATE rating SET count_lose = {res} WHERE login = '{login}'")
+            self.conn.commit()
 
-            self.cur.execute(f"UPDATE rating SET count_lose = {count_lose}")
+    """
+            Метод для получения рейтинга на пользователся в бд
+    """
+
+    def getRecord(self, login):
+        self.cur.execute(f"SELECT count_win, count_lose FROM rating WHERE login = '{login}'")
+        return self.cur.fetchone()
+
+    """
+            Метод для получения всех записей из бд рейтинг
+    """
+
+    def getAllRecord(self):
+        self.cur.execute(f"SELECT login, count_win, count_lose FROM rating ORDER BY count_win DESC")
+        return self.cur.fetchall()
+
+    """
+            Метод для вывода в консоль всех записей пользователей в бд
+    """
 
     def printRegister(self):
         date = datebase()
         date.cur.execute("SELECT * FROM register")
         print(date.cur.fetchall())
+
+    """
+            Метод для вывода в консоль всех рекордов пользователей в бд
+    """
 
     def printRecords(self):
         date = datebase()
@@ -64,8 +108,12 @@ class datebase:
         print(date.cur.fetchall())
 
 
-if __name__ == '__main__':
-    date = datebase()
-    date.cur.execute("SELECT * FROM register")
-    # date.conn.commit()
-    print(date.cur.fetchall())
+# if __name__ == '__main__':
+#     date = datebase()
+#     date.cur.execute(f"UPDATE rating SET count_win = {0}")
+#     date.conn.commit()
+#     date.cur.execute(f"UPDATE rating SET count_lose = {0}")
+#     date.conn.commit()
+#     # date.cur.execute("INSERT INTO rating VALUES (5, 'Second player',0,0 )")
+#     # date.conn.commit()
+#     print(date.cur.fetchall())
--- mainWindow.py
+++ mainWindow.py
@@ -1,4 +1,6 @@
 from tkinter import *
+
+import db_scripts
 from ChildWindow import ChildWindows
 from GameWindow import GameWindow
 
@@ -18,10 +20,15 @@ class Window:
         self.root.title(title)
         self.root.geometry(f"{width}x{height}+400+200")
         self.root.resizable(width=False, height=False)
+        self.firstUserName = ""
+        self.secondUserName = ""
+        db = db_scripts.datebase()
+        self.rating = db.getAllRecord()
 
     """
         Метод для запуска объекта окна Window
     """
+
     def run(self):
         self.draw_widgets()
         self.root.mainloop()
@@ -29,15 +36,32 @@ class Window:
     """
         Метод для отрисовки виджетов
     """
+
     def draw_widgets(self):
-        btn1 = Button(self.root, width=30, height=5, text="Играть", command=self.create_child_login)
-        btn2 = Button(self.root, width=30, height=5, text="Рейтинг", command=self.create_child_rat)
-        btn1.pack()
-        btn2.pack()
+        btnPlay = Button(self.root, width=50, height=3, text="Играть", command=self.create_child_login)
+        btnUpdRat = Button(self.root, width=15, height=2, text="Обновить рейтинг", command=self.update_rating)
+        listRatLogin = Listbox()
+        listRatWin = Listbox()
+        listRatLose = Listbox()
+        listRatLogin.insert(END," Имя")
+        listRatWin.insert(END, " Победы")
+        listRatLose.insert(END, " Поражения")
+        for res in self.rating:
+            listRatLogin.insert(END, " " + res[0])
+        for res in self.rating:
+            listRatWin.insert(END, " " + str(res[1]))
+        for res in self.rating:
+            listRatLose.insert(END, " " + str(res[2]))
+        btnPlay.place(relx=.3, rely=.1)
+        btnUpdRat.place(relx=.5, rely=.3)
+        listRatLogin.place(relx=.4, rely=.42, width=50, height=140)
+        listRatWin.place(relx=.5, rely=.42, width=60, height=140)
+        listRatLose.place(relx=.6, rely=.42, width=80, height=140)
 
     """
             Метод вызова дочерного окна для входа
     """
+
     def create_child_login(self):
         self.firstLog = ChildWindows(self.root, 300, 200, "Войти", 1)
         self.firstValue = self.firstLog.getUser(1)
@@ -55,11 +79,21 @@ class Window:
         else:
             print("1 - its not worked")
 
-    def create_child_rat(self):
-        print("Rating started")
+    """
+            Метод для обновления рейтинга
+    """
+
+    def update_rating(self):
+        db = db_scripts.datebase()
+        self.rating = db.getAllRecord()
+        self.draw_widgets()
+
+    """
+            Метод для вызова окна с игрой
+    """
 
     def create_child_game(self):
         print("first player - " + str(self.firstUserName))
         print("second player - " + str(self.secondUserName))
-        self.Game = GameWindow(self.root, 300, 200, "Игра", self.firstUserName, self.secondUserName)
+        self.Game = GameWindow(self.root, 700, 300, "Игра", self.firstUserName, self.secondUserName)
         print("Game started")
