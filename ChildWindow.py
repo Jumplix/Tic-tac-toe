e0b75115583f83be51a18851b50ee68bc4ee0606
Wurberry <dany.evstigneev@gmail.com>
2021-04-24 16:35:36 +0300

    Доработан алгоритм "игрок и бот" и правки в дизайне

--- ChildWindow.py
+++ ChildWindow.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 import db_scripts
+from PIL import ImageTk, Image
 
 """
     Для создания дочернего окна используется класс ChildWindows
@@ -17,6 +18,8 @@ class ChildWindows:
         self.root.title(title)
         self.root.geometry(f"{width}x{height}+600+300")
         self.root.resizable(False, False)
+        self.res = 3
+        self.msgtext = ""
 
         if plnumb == 1:
             self.firstUser = True
@@ -24,13 +27,6 @@ class ChildWindows:
         if plnumb == 2:
             self.firstUser = False
             self.secondUser = True
-
-        self.labelLogin = Label(self.root, text='Логин')
-        self.textLogin = Entry(self.root, width=30)
-        self.labelPassword = Label(self.root, text='Пароль')
-        self.textPassword = Entry(self.root, width=30, show="*")
-        self.signIn = Button(self.root, text='Войти', command=self.checkLog)
-        self.signUp = Button(self.root, text='Зарегистрироваться', command=self.checkReg)
 
         self.draw_widgets_child()
         self.grab_focus()
@@ -47,18 +43,27 @@ class ChildWindows:
             Метод отрисовки виджетов дочернего окна
     """
     def draw_widgets_child(self):
-
         if self.firstUser is True:
-            self.labelLogin = Label(self.root, text='Игрок 1')
+            self.labelUsName = Label(self.root, text='Игрок 1')
         elif self.secondUser is True:
-            self.labelLogin = Label(self.root, text='Игрок 2')
+            self.labelUsName = Label(self.root, text='Игрок 2')
 
-        self.labelLogin.pack()
-        self.textLogin.pack()
-        self.labelPassword.pack()
-        self.textPassword.pack()
-        self.signIn.pack()
-        self.signUp.pack()
+        self.labelLogin = Label(self.root, text='Логин')
+        self.textLogin = Entry(self.root, width=30)
+        self.labelPassword = Label(self.root, text='Пароль')
+        self.textPassword = Entry(self.root, width=30, show="*")
+        self.signIn = Button(self.root, text='Войти', command=self.checkLog)
+        self.signUp = Button(self.root, text='Зарегистрироваться', command=self.checkReg)
+        self.labelMessage = Label(self.root, width=50, text=self.msgtext)
+
+        self.labelUsName.place(relx=.45, rely=.1)
+        self.labelLogin.place(relx=.45, rely=.2)
+        self.textLogin.place(relx=.32, rely=.3)
+        self.labelPassword.place(relx=.45, rely=.4)
+        self.textPassword.place(relx=.32, rely=.5)
+        self.signIn.place(relx=.3, rely=.6)
+        self.signUp.place(relx=.45, rely=.6)
+        self.labelMessage.place(relx=.1, rely=.7)
 
     """
             Метод для проверки входа пользователей в бд
@@ -67,16 +72,19 @@ class ChildWindows:
     def checkLog(self):
         db = db_scripts.datebase()
         bcheck = db.checkUser(self.textLogin.get(), self.textPassword.get())
-        if bcheck and self.firstUser is True:
+        if bcheck == 1 and self.firstUser:
             print("Entered First")
             self.firstUser = True
             self.firstUserName = self.textLogin.get()
             self.root.destroy()
-        if bcheck and self.secondUser is True:
+        if bcheck == 1 and self.secondUser:
             print("Entered Second")
             self.secondUser = True
             self.secondUserName = self.textLogin.get()
             self.root.destroy()
+        if bcheck == 0:
+            self.msgtext = "Неверный логин или пароль ИЛИ вы не зарегестрированы"
+            self.draw_widgets_child()
         db.printRegister()
 
     """
@@ -85,8 +93,16 @@ class ChildWindows:
 
     def checkReg(self):
         db = db_scripts.datebase()
-        db.addUser(self.textLogin.get(), self.textPassword.get())
+        if self.textLogin.get() != "Bot" and (self.textPassword.get() != "" or self.textPassword.get() != "") and self.textLogin.get() != "bot":
+            self.res = db.addUser(self.textLogin.get(), self.textPassword.get())
+        if self.textLogin.get() == "Bot" and self.textLogin.get() == "bot":
+            print("Зарезервированное имя пользователя")
         db.printRegister()
+        if self.res == 1:
+            self.msgtext = ""
+        if self.res == 0:
+            self.msgtext = "Имя занято"
+            self.draw_widgets_child()
 
     """
             Метод для проверки вошел ли пользователь
@@ -104,6 +120,6 @@ class ChildWindows:
 
     def getUserName(self, posi):
         if posi == 1:
-            return  self.firstUserName
+            return self.firstUserName
         elif posi == 2:
             return self.secondUserName
--- mainWindow.py
+++ mainWindow.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import *
 
 import db_scripts
@@ -10,12 +11,11 @@ from GameWindow import GameWindow
     window = Window()
 """
 
-
 class Window:
     """
         Конструктор класса Window
     """
-    def __init__(self, title="Крестики-нолики", width=700, height=400):
+    def __init__(self, title="Крестики-нолики", width=600, height=400):
         self.root = Tk()
         self.root.title(title)
         self.root.geometry(f"{width}x{height}+400+200")
@@ -38,7 +38,8 @@ class Window:
     """
 
     def draw_widgets(self):
-        btnPlay = Button(self.root, width=50, height=3, text="Играть", command=self.create_child_login)
+        btnPlay = Button(self.root, width=25, height=3, text="Играть с игроком", command=self.create_child_login)
+        btnPlayBot = Button(self.root, width=25, height=3, text="Играть с ботом", command=self.create_logBot)
         btnUpdRat = Button(self.root, width=15, height=2, text="Обновить рейтинг", command=self.update_rating)
         listRatLogin = Listbox()
         listRatWin = Listbox()
@@ -52,32 +53,48 @@ class Window:
             listRatWin.insert(END, " " + str(res[1]))
         for res in self.rating:
             listRatLose.insert(END, " " + str(res[2]))
-        btnPlay.place(relx=.3, rely=.1)
-        btnUpdRat.place(relx=.5, rely=.3)
-        listRatLogin.place(relx=.4, rely=.42, width=50, height=140)
-        listRatWin.place(relx=.5, rely=.42, width=60, height=140)
-        listRatLose.place(relx=.6, rely=.42, width=80, height=140)
+        btnPlay.place(relx=.17, rely=.1)
+        btnPlayBot.place(relx=.52, rely=.1)
+        btnUpdRat.place(relx=.4, rely=.3)
+        listRatLogin.place(relx=.35, rely=.42, width=60, height=140)
+        listRatWin.place(relx=.45, rely=.42, width=70, height=140)
+        listRatLose.place(relx=.55, rely=.42, width=80, height=140)
 
     """
             Метод вызова дочерного окна для входа
     """
 
     def create_child_login(self):
-        self.firstLog = ChildWindows(self.root, 300, 200, "Войти", 1)
+        self.firstLog = ChildWindows(self.root, 500, 300, "Войти", 1)
         self.firstValue = self.firstLog.getUser(1)
         if self.firstValue is True:
             self.firstValue = False
             print("its worked")
             self.firstUserName = self.firstLog.getUserName(1)
-            self.secondLog = ChildWindows(self.root, 300, 200, "Войти", 2)
+            self.secondLog = ChildWindows(self.root, 500, 300, "Войти", 2)
             self.secondValue = self.secondLog.getUser(2)
             if self.secondValue is True:
                 self.secondUserName = self.secondLog.getUserName(2)
-                self.create_child_game()
+                self.create_child_game("std")
             else:
                 print("2 - its not worked")
         else:
             print("1 - its not worked")
+
+    """
+            Создание окна для регистрации игры с боту
+    """
+
+    def create_logBot(self):
+        self.firstLog = ChildWindows(self.root, 500, 300, "Войти", 1)
+        self.firstValue = self.firstLog.getUser(1)
+        if self.firstValue is True:
+            self.firstValue = False
+            print("its worked")
+            self.firstUserName = self.firstLog.getUserName(1)
+            self.create_child_game("bot")
+        else:
+            print("1 - its not worked BOOOT")
 
     """
             Метод для обновления рейтинга
@@ -92,8 +109,11 @@ class Window:
             Метод для вызова окна с игрой
     """
 
-    def create_child_game(self):
+    def create_child_game(self, type):
         print("first player - " + str(self.firstUserName))
         print("second player - " + str(self.secondUserName))
-        self.Game = GameWindow(self.root, 700, 300, "Игра", self.firstUserName, self.secondUserName)
         print("Game started")
+        if type == "std":
+            self.Game = GameWindow(self.root, 700, 300, "Игра", self.firstUserName, self.secondUserName)
+        elif type == "bot":
+            self.Game = GameWindow(self.root, 700, 300, "Игра", self.firstUserName, "Bot")
